d4cfdeb727e3504f869a0a624d6a447c1c55931b
Severino Carlos <severino.carlos@vallorizasolucoes.com.br>
2024-07-15 14:10:53 -0300

    feat(header): adding header component (#13)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginService } from './services/login/login.service';
+import { HeaderComponent } from './components/header/header.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   private readonly loginService = inject(LoginService);
-  private readonly isAuthenticated = this.loginService.isAuthenticated$;
+  readonly isAuthenticated = this.loginService.isAuthenticated$;
 }
