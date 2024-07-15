48950e430c1a6edcf27d4f78189bc914c5a7170e
Severino Carlos <severino.carlos@vallorizasolucoes.com.br>
2024-07-15 15:11:06 -0300

    feat(footer): adding footer component (#14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { LoginService } from './services/login/login.service';
 import { HeaderComponent } from './components/header/header.component';
 import { AsyncPipe } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    AsyncPipe,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
