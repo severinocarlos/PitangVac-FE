01e6120c745d82047e81e41792ac1f89bbedc3f5
severinocarlos <severinocsilva3@gmail.com>
2024-07-16 00:55:05 -0300

    feat(guards): adding route guards (#18)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,32 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { SchedulesComponent } from './pages/schedules/schedules.component';
+import { loggedGuard } from './guards/logged.guard';
+import { authenticateGuard } from './guards/authenticate.guard';
 
 export const routes: Routes = [
     {
         path: "",
         redirectTo: "login",
-        pathMatch: 'full'
+        pathMatch: 'full',
     },
     {
         path: "login",
         component: LoginComponent,
-        pathMatch: 'full'
+        pathMatch: 'full',
+        canActivate: [loggedGuard]
     },
     {
         path: "cadastrar",
         component: RegisterComponent,
-        pathMatch: 'full'
+        pathMatch: 'full',
+        canActivate: [loggedGuard]
+    },
+    {
+        path: "agendamentos",
+        component: SchedulesComponent,
+        pathMatch: 'full',
+        canActivate: [authenticateGuard]
     }
 ];
